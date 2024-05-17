37a34f148f960c9b944d0721e01c7bd5818228e5
matt <matt1235@naver.com>
2024-05-17 16:36:09 +0900

    initial commit

---
+++ src/test/java/com/example/feedserver/FeedServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.feedserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
