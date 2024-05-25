2f92ccf2c04d1a6c212347e3f687cf8135b86d7f
matt <matt1235@naver.com>
2024-05-25 14:52:46 +0900

    feat: 카프카 기본 설정 추가 (#7)

---
+++ src/main/java/com/example/feedserver/config/KafkaConfiguration.java
@@ -0,0 +1,14 @@
+package com.example.feedserver.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaConfiguration {
+
+    @Bean
+    public NewTopic feedTopic() {
+        return new NewTopic("feed.created", 1, (short)1);
+    }
+}
